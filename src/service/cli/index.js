2ad84710ef67ac3df5aa9a9e81f3709e028abaa8
Eugene <63777417+nadProg@users.noreply.github.com>
2022-01-23 10:28:51 +0700

    Update Cli

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,15 +1,11 @@
-'use strict';
+"use strict";
 
-const version = require(`./version`);
 const help = require(`./help`);
+const version = require(`./version`);
 const generate = require(`./generate`);
 
-const Cli = {
-  [version.name]: version,
-  [help.name]: help,
-  [generate.name]: generate,
-};
-
 module.exports = {
-  Cli,
+  [help.name]: help,
+  [version.name]: version,
+  [generate.name]: generate,
 };
