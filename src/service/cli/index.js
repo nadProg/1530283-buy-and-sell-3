84adb7749520612e9cc05636abc90a271ba1991b
Eugene <63777417+nadProg@users.noreply.github.com>
2022-01-23 19:25:35 +0700

    Add server

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -3,9 +3,11 @@
 const help = require(`./help`);
 const version = require(`./version`);
 const generate = require(`./generate`);
+const server = require(`./server`);
 
 module.exports = {
   [help.name]: help,
   [version.name]: version,
   [generate.name]: generate,
+  [server.name]: server,
 };
---
+++ src/service/cli/server.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const chalk = require(`chalk`);
+const http = require(`http`);
+const fs = require(`fs`).promises;
+
+const DEFAULT_PORT = 3000;
+const FILENAME = `mocks.json`;
+
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
+
+const sendResponse = (res, statusCode, message) => {
+  const template = `
+    <!Doctype html>
+      <html lang="ru">
+      <head>
+        <title>With love from Node</title>
+      </head>
+      <body>${message}</body>
+    </html>`.trim();
+
+  res.writeHead(statusCode, {
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+
+  res.end(template);
+};
+
+const onClientConnect = async (req, res) => {
+  const notFoundMessageText = `Not found`;
+
+  switch (req.url) {
+    case `/`:
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
+        sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
+      } catch (err) {
+        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      }
+
+      break;
+    default:
+      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
+      break;
+  }
+};
+
+module.exports = {
+  name: `--server`,
+  run(args) {
+    const [rawPort] = args;
+    const port = Number.parseInt(rawPort, 10) || DEFAULT_PORT;
+
+    http.createServer(onClientConnect)
+    .listen(port)
+    .on(`listening`, () => {
+      console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    })
+    .on(`error`, ({message}) => {
+      console.error(chalk.red(`Ошибка при создании сервера: ${message}`));
+    });
+  }
+};
